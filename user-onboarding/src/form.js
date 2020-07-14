af8d1ab5c9d05e1a0881e21981fad36eef1e18d8
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-07-14 19:56:54 -0400

    working on validation.

--- user-onboarding/src/form.js
+++ user-onboarding/src/form.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as yup from 'yup';
+import axios from 'axios';
 
 export default function Form() {
      
@@ -20,41 +21,70 @@ export default function Form() {
         password: yup.string().required(8, "Minimum of 8 characters required."),
         terms: yup.boolean().oneOf([true], "Please agree to terms and conditions.")
     });
-    
-    return (
-        // <div>Form starts here!</div>
 
-        <form>
-            <input
-                type='text'
-                name='name'
-                // onChange={inputChange}
-                // value={formState.name}
-                label="Name"
-            />
-            <input 
-                type='text'
-                name='email'
-                // onChange={inputChange}
-                // value={formState.name}
-                label="Email"
-            />
-            <input
-                type='text'
-                name='password'
-                // onChange={inputChange}
-                // value={formState.name}
-                label="Email"
-            />
-            <label className="terms" htmlFor="terms">
-                <input name="terms" type="checkbox" />     
-               [ //add inputChange ]
+    useEffect(() => {
+        if (formState.terms) {
+            setButtonDisabled(!formState.terms);
+        }
+    }, [formState]);
+
+    const formSubmit = e => {
+        e.preventDefault();
+        console.log("form submitted!");
+        axios
+            .post("https://reqres.in/api/users", formState)
+            .then(() => console.log("form submitted success"))
+            .catch(err => console.log(err));
+    };
+
+    const validateChange = e => {
+        yup.reach(formSchema, e.target.name)
+        validateChange(e.target.value)
+            .then(valid =>
+                setErrors({
+                    ...errors, [e.target.name]: ""
+                })
+            )
+            .catch(error =>
+                setErrors({
+                    ...errors, [e.target.name]: error.errors[0]
+                })
+            );
+    }
+        return (
+            // <div>Form starts here!</div>
+
+            <form>
+                <input
+                    type='text'
+                    name='name'
+                    // onChange={inputChange}
+                    // value={formState.name}
+                    label="Name"
+                />
+                <input
+                    type='text'
+                    name='email'
+                    // onChange={inputChange}
+                    // value={formState.name}
+                    label="Email"
+                />
+                <input
+                    type='text'
+                    name='password'
+                    // onChange={inputChange}
+                    // value={formState.name}
+                    label="Email"
+                />
+                <label className="terms" htmlFor="terms">
+                    <input name="terms" type="checkbox" />
+               
                  Terms & Conditions
                  </label>
-            <button>Submit</button>  
-            [//add disable button]
+                <button>Submit</button>
+            
 
-        </form>
+            </form>
 
-    )
-};
+        );
+    }
